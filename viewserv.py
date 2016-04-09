9fe72f735cdccff50ca0dbd4fe605351a3dfecb1
rudetrooper <rudetrooper@bluedevs.net>
2016-04-09 14:44:59 -0400

    Added files via upload

--- viewserv.py
+++ viewserv.py
@@ -1,11 +1,15 @@
-from flask import jsonify, Flask, render_template, request
+from flask import jsonify, Flask, render_template, request, requests
 
 app = Flask(__name__)
 
+#API_key= 'AIzaSyBPyOr8dQoKhti7HW6w02oYtTkdgfScPaM'
 @app.route('/locate', methods=['POST'])
 def locate():
+	coord = {'Latitude': str(request.json['lat']),'Longitude': str(request.json['long'])}
     print("lat: " + str(request.json['lat']))
     print("long: " + str(request.json['long']))
+    
+    state_location = requests.get('https://maps.googleapis.com/maps/api/geocode/json?latlng={Latitude},{Longitude}&key=AIzaSyBPyOr8dQoKhti7HW6w02oYtTkdgfScPaM'.format(**coord))
     return jsonify({'title': request.json['lat'],'content': request.json['long']})
 
 @app.route('/', methods=['GET'])
