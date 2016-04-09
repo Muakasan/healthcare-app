3c3efa220dbe4389d88a0e8aca69e24e1919d2a5
muakasan <muakasan@gmail.com>
2016-04-09 09:04:48 -0400

    added basic server code

--- viewserv.py
+++ viewserv.py
@@ -1,6 +1,11 @@
-from flask import jsonify
+from flask import jsonify, Flask
+
+app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-
 def locate():
-	return jsonify({'test':return.form['lat'],return.form['long']})
+    print(request.json['lat'])
+    return jsonify({'title':request.json['lat'],'content': request.json['long']})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
