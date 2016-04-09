90e76951094937c6a4a2003df82644a488773e57
rudetrooper <rudetrooper@bluedevs.net>
2016-04-09 15:30:29 -0400

    Added files via upload

--- viewserv.py
+++ viewserv.py
@@ -1,15 +1,17 @@
-from flask import jsonify, Flask, render_template, request, requests
+from flask import jsonify, Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 #API_key= 'AIzaSyBPyOr8dQoKhti7HW6w02oYtTkdgfScPaM'
 @app.route('/locate', methods=['POST'])
 def locate():
-	coord = {'Latitude': str(request.json['lat']),'Longitude': str(request.json['long'])}
+    coord = {'Latitude': str(request.json['lat']),'Longitude': str(request.json['long'])}
     print("lat: " + str(request.json['lat']))
     print("long: " + str(request.json['long']))
     
-    state_location = requests.get('https://maps.googleapis.com/maps/api/geocode/json?latlng={Latitude},{Longitude}&key=AIzaSyBPyOr8dQoKhti7HW6w02oYtTkdgfScPaM'.format(**coord))
+    USstate = requests.get('https://maps.googleapis.com/maps/api/geocode/json?latlng={Latitude},{Longitude}&key=AIzaSyBPyOr8dQoKhti7HW6w02oYtTkdgfScPaM'.format(**coord))
+    #print USstate.headers()
     return jsonify({'title': request.json['lat'],'content': request.json['long']})
 
 @app.route('/', methods=['GET'])
