a2062bd3b931e3d2f8d9f1f278d6bdbacb83bbe6
muakasan <muakasan@gmail.com>
2016-04-09 09:09:34 -0400

    added template

--- viewserv.py
+++ viewserv.py
@@ -1,11 +1,15 @@
-from flask import jsonify, Flask
+from flask import jsonify, Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/locate', methods=['POST'])
 def locate():
     print(request.json['lat'])
     return jsonify({'title':request.json['lat'],'content': request.json['long']})
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
